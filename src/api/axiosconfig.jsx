894611a6ba95e1b8a754724dd0bc335de562ae88
Aman-Pal287 <palaman9786@gmail.com>
2025-07-25 02:32:18 +0530

    base url of axios

--- src/api/axiosconfig.jsx
+++ src/api/axiosconfig.jsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5173/",
+  baseURL: "https://hackathon-project-0-1.vercel.app/",
 });
+
+//! edits
+// const instance = axios.create({
+//   baseURL: "http://localhost:5173/",
+// });
 
 export default instance;
