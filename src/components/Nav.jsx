209715188a4966aabbfd9565030d3dc48f3764c9
Aman-Pal287 <palaman9786@gmail.com>
2025-07-25 20:46:56 +0530

    navbar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
 import "./Nav.scss";
 
 const Nav = () => {
@@ -7,6 +8,11 @@ const Nav = () => {
   const isHomePage = location.pathname === "/";
   const isAboutPage = location.pathname === "/about";
   const user = useSelector((state) => state.userReducer.users);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav
@@ -14,8 +20,14 @@ const Nav = () => {
         isHomePage || isAboutPage ? "transparent-navbar" : ""
       }`}
     >
-      <h1>KStyle Studio</h1>
-      <div className="nav-links">
+      <div className="navbar-header">
+        <h1>KStyle Studio</h1>
+        <div className="hamburger" onClick={toggleMenu}>
+          <i class="ri-menu-line"></i>
+        </div>
+      </div>
+
+      <div className={`nav-links ${menuOpen ? "show" : ""}`}>
         <NavLink
           to="/"
           className={({ isActive }) => (isActive ? "active" : "")}
@@ -48,13 +60,13 @@ const Nav = () => {
               to="/admin/user-profile"
               className={({ isActive }) => (isActive ? "active" : "")}
             >
-              Settings
+              <i class="ri-user-3-fill"></i>
             </NavLink>
             <NavLink
               to="/cart"
               className={({ isActive }) => (isActive ? "active" : "")}
             >
-              Cart
+              <i class="ri-shopping-cart-line"></i>
             </NavLink>
           </>
         ) : (
@@ -71,3 +83,77 @@ const Nav = () => {
 };
 
 export default Nav;
+
+// import { useSelector } from "react-redux";
+// import { NavLink, useLocation, useNavigate } from "react-router-dom";
+// import "./Nav.scss";
+
+// const Nav = () => {
+//   const location = useLocation();
+//   const isHomePage = location.pathname === "/";
+//   const isAboutPage = location.pathname === "/about";
+//   const user = useSelector((state) => state.userReducer.users);
+
+//   return (
+//     <nav
+//       className={`custom-navbar ${
+//         isHomePage || isAboutPage ? "transparent-navbar" : ""
+//       }`}
+//     >
+//       <h1>KStyle Studio</h1>
+//       <div className="nav-links">
+//         <NavLink
+//           to="/"
+//           className={({ isActive }) => (isActive ? "active" : "")}
+//         >
+//           Home
+//         </NavLink>
+//         <NavLink
+//           to="/about"
+//           className={({ isActive }) => (isActive ? "active" : "")}
+//         >
+//           About
+//         </NavLink>
+//         <NavLink
+//           to="/products"
+//           className={({ isActive }) => (isActive ? "active" : "")}
+//         >
+//           Products
+//         </NavLink>
+//         {user ? (
+//           <>
+//             {user?.isAdmin && (
+//               <NavLink
+//                 to="/admin/create-product"
+//                 className={({ isActive }) => (isActive ? "active" : "")}
+//               >
+//                 Create Product
+//               </NavLink>
+//             )}
+//             <NavLink
+//               to="/admin/user-profile"
+//               className={({ isActive }) => (isActive ? "active" : "")}
+//             >
+//               Settings
+//             </NavLink>
+//             <NavLink
+//               to="/cart"
+//               className={({ isActive }) => (isActive ? "active" : "")}
+//             >
+//               Cart
+//             </NavLink>
+//           </>
+//         ) : (
+//           <NavLink
+//             to="/login"
+//             className={({ isActive }) => (isActive ? "active" : "")}
+//           >
+//             Login
+//           </NavLink>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Nav;
